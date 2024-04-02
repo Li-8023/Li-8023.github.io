bb63187476bc036cdb4245817c01aaeb37e4cbeb
Li He <80284104+Li-8023@users.noreply.github.com>
2024-04-02 16:40:24 +0800

    animation

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//https://www.youtube.com/watch?v=H2HYccAGR00
+// Get reference to our elements
+let number = document.getElementById('number');
+let counter = 0;
+
+setInterval(() => {
+    if(counter == 65){
+        clearInterval;
+    }else{
+        counter += 1;
+        number.innerHTML = `${counter}%`;
+    }
+}, 30);
